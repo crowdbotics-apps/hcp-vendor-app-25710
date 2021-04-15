155383bc7376119ac92e3a990b325dbb8a5074e2
Crowdbotics <team@crowdbotics.com>
2021-04-15 03:41:19 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/hcp_vendor_app_25710/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hcp_vendor_app_25710;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "hcp_vendor_app_25710";
+  }
+}
---
+++ backend/hcp_vendor_app_25710/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for hcp_vendor_app_25710 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hcp_vendor_app_25710.settings')
+
+application = get_wsgi_application()
